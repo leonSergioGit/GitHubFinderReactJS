a1f4cdbb8c5d37c3058f00c98bfd70896be082eb
unknown <leongit1990@gmail.com>
2020-01-07 08:36:08 +0100

    Refactored with hooks and context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,24 +1,20 @@
-import React, { Fragment, Component } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext'
 
-class User extends Component {
-    componentDidMount(){
-        this.props.getUser(this.props.match.params.login);
-        this.props.getUserRepos(this.props.match.params.login);
-    }
+const User = ({ match }) => {
+    const githubContext = useContext(GithubContext);
 
-    static propTypes = {
-        loading: PropTypes.bool,
-        user: PropTypes.object.isRequired,
-        repos: PropTypes.array.isRequired,
-        getUser: PropTypes.func.isRequired,
-        getUserRepos: PropTypes.func.isRequired
-    }
+    const { getUser, loading, user, repos, getUserRepos } = githubContext;
 
-    render() {
+    //Use effect runs in every update. In order to avoid that, we need to add the brackets at the end
+    useEffect(() => {
+        getUser(match.params.login);
+        getUserRepos(match.params.login);
+    }, []);
+
         const { 
             name, 
             company,
@@ -33,9 +29,8 @@ class User extends Component {
             public_repos,
             public_gists,
             hireable
-            } = this.props.user;
+            } = user;
 
-            const { loading, repos } = this.props;
 
             if(loading) return <Spinner />;
 
@@ -96,6 +91,7 @@ class User extends Component {
         </Fragment>
         );
     }
-}
+
+
 
 export default User
